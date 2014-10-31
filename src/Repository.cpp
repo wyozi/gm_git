7fd5f765a3db454a44d3178831a3e55bd343305a
wyozi <joonasmertanen@gmail.com>
2014-10-31 18:35:37 +0200

    Finish untested version of repo->GetStatus()

--- src/Repository.cpp
+++ src/Repository.cpp
@@ -11,15 +11,104 @@ Repository::~Repository() {
 	this->Free();
 }
 
+std::vector<RepositoryStatusEntry*> GetFilteredStatuses(git_status_list* list, std::function<bool(const git_status_entry*, RepositoryStatusEntry*)> f) {
+	std::vector<RepositoryStatusEntry*> vec;
+	size_t count = git_status_list_entrycount(list);
+	
+	for (size_t i=0; i<count; ++i) {
+		const git_status_entry *s = git_status_byindex(list, i);
+		RepositoryStatusEntry* entry;
+		if (f(s, entry))
+			vec.push_back(entry);
+	}
+
+	return vec;
+}
+
 RepositoryStatus* Repository::GetStatus() {
 	RepositoryStatus* status;
+	int error;
 
+	// Get current branch
 	git_reference* head;
-
-	int error = git_repository_head(&head, repo);
+	error = git_repository_head(&head, repo);
 	if (error < 0) throw GitError(error);
 
 	status->branch = std::string(git_reference_shorthand(head));
+
+	// Get all changes in index, workdir etc
+	git_status_options opts = GIT_STATUS_OPTIONS_INIT;
+	opts.show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
+	opts.flags = GIT_STATUS_OPT_INCLUDE_UNTRACKED |
+				 GIT_STATUS_OPT_RENAMES_HEAD_TO_INDEX |
+				 GIT_STATUS_OPT_SORT_CASE_SENSITIVELY;
+
+	git_status_list *statuses = NULL;
+	error = git_status_list_new(&statuses, repo, &opts);
+	if (error < 0) throw GitError(error);
+
+	// Index changes (aka changes to be committed)
+	auto indexChanges = GetFilteredStatuses(statuses, [](const git_status_entry* s, RepositoryStatusEntry* rse) -> bool {
+		if (s->status == GIT_STATUS_CURRENT)
+			return false;
+
+		int type = NULL;
+		switch(s->status) {
+		case GIT_STATUS_INDEX_NEW:
+			type = ChangeType::NEW; break;
+		case GIT_STATUS_INDEX_MODIFIED:
+			type = ChangeType::MODIFIED; break;
+		case GIT_STATUS_INDEX_DELETED:
+			type = ChangeType::DELETED; break;
+		case GIT_STATUS_INDEX_RENAMED:
+			type = ChangeType::RENAMED; break;
+		case GIT_STATUS_INDEX_TYPECHANGE:
+			type = ChangeType::TYPECHANGE; break;
+		}
+
+		if (type == NULL)
+			return false;
+
+		rse->type = type;
+
+		rse->path = s->head_to_index->new_file.path;
+		rse->old_path = s->head_to_index->old_file.path;
+
+		return true;
+	});
+	status->index_changes = indexChanges;
+	
+	// Workdirectory changes (aka changes that are _not_ staged for commit)
+	// Note: workdir changes with status of GIT_STATUS_WT_NEW are untracked
+	auto workdirChanges = GetFilteredStatuses(statuses, [](const git_status_entry* s, RepositoryStatusEntry* rse) -> bool {
+		if (s->status == GIT_STATUS_CURRENT || s->index_to_workdir == NULL)
+			return false;
+
+		int type = NULL;
+		switch(s->status) {
+		case GIT_STATUS_WT_NEW:
+			type = ChangeType::NEW; break;
+		case GIT_STATUS_WT_MODIFIED:
+			type = ChangeType::MODIFIED; break;
+		case GIT_STATUS_WT_DELETED:
+			type = ChangeType::DELETED; break;
+		case GIT_STATUS_WT_RENAMED:
+			type = ChangeType::RENAMED; break;
+		case GIT_STATUS_WT_TYPECHANGE:
+			type = ChangeType::TYPECHANGE; break;
+		}
+
+		if (type == NULL)
+			return false;
+
+		rse->type = type;
+
+		rse->path = s->index_to_workdir->new_file.path;
+		rse->old_path = s->index_to_workdir->old_file.path;
+
+		return true;
+	});
+	status->work_dir_changes = workdirChanges;
 
 	return status;
 }
--- src/Repository.h
+++ src/Repository.h
@@ -1,10 +1,29 @@
 #pragma once
 
 #include <stdlib.h>
+#include <functional>
+
 #include "Util.h"
 
+namespace ChangeType {
+	enum {
+		NEW,
+		MODIFIED,
+		DELETED,
+		RENAMED,
+		TYPECHANGE
+	};
+}
+
+struct RepositoryStatusEntry {
+	std::string path;
+	std::string old_path;
+	int type;
+};
 struct RepositoryStatus {
 	std::string branch;
+	std::vector<RepositoryStatusEntry*> index_changes;
+	std::vector<RepositoryStatusEntry*> work_dir_changes;
 };
 
 class Repository {
