a07356b1a7fd31b2025362d29dfe6dca2e7acc9c
wyozi <joonasmertanen@gmail.com>
2014-10-31 19:24:22 +0200

    Index changes added to repo:Status()

--- src/LuaBridge.cpp
+++ src/LuaBridge.cpp
@@ -13,7 +13,14 @@ int LuaBridge::Status(lua_State* state) {
 	if (!repo)
 		return 0;
 
-	RepositoryStatus* status = repo->GetStatus();
+	RepositoryStatus* status;
+	try {
+		status = repo->GetStatus();
+	} catch (GitError e) {
+		LUA->PushBool(false);
+		LUA->PushString(Wyozi::Util::GitErrorToString(e.error).c_str());
+		return 2;
+	}
 	
 	LUA->CreateTable();
 
@@ -22,6 +29,37 @@ int LuaBridge::Status(lua_State* state) {
 			LUA->PushString(status->branch.c_str());
 		else
 			LUA->PushNil();
+		LUA->SetTable(-3);
+		
+		LUA->PushString("IndexChanges");
+		LUA->CreateTable();
+		
+		int i = 1;
+		for (auto change = status->index_changes.begin(); change != status->index_changes.end(); ++change) {
+			std::string old_path = (*change)->old_path;
+			std::string new_path = (*change)->path;
+			int type = (*change)->type;
+
+			LUA->PushNumber(i);
+			LUA->CreateTable();
+
+				LUA->PushString("Status");
+				LUA->PushNumber(type);
+				LUA->SetTable(-3);
+
+				LUA->PushString("Path");
+				LUA->PushString(new_path.c_str());
+				LUA->SetTable(-3);
+
+				if (!old_path.empty() && old_path != new_path) {
+					LUA->PushString("OldPath");
+					LUA->PushString(old_path.c_str());
+					LUA->SetTable(-3);
+				}
+			
+			LUA->SetTable(-3);
+			i++;
+		}
 		LUA->SetTable(-3);
 
 	return 1;
